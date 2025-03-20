b3883a8bdeaac415d761841fc248f689554b31a3
JhonFy <dijhonsdeveloper1023@gmail.com>
2025-03-19 22:31:13 -0700

    FEAT: Reordenar columnas del DataGridView en FrmUsers - 2025-03-19 22:41:31

--- CapaPresentacion/frmUsers.cs
+++ CapaPresentacion/frmUsers.cs
@@ -43,13 +43,12 @@ namespace CapaPresentacion
                 // Ocultar la columna vacía a la izquierda
                 dataGridViewUsuarios.RowHeadersVisible = false;
 
-
                 // // Personalizar las columnas visibles
                 dataGridViewUsuarios.Columns["IdUsuario"].HeaderText = "Entry Numbe";
                 dataGridViewUsuarios.Columns["DOCUMENTO"].HeaderText = "Document";
                 dataGridViewUsuarios.Columns["NOMBRECOMPLETO"].HeaderText = "Full Name";
-                dataGridViewUsuarios.Columns["CORREO"].HeaderText = "Mail";
                 dataGridViewUsuarios.Columns["CLAVE"].HeaderText = "Password";
+                dataGridViewUsuarios.Columns["CORREO"].HeaderText = "Mail";
                 //Agregar columna de Rol
                 dataGridViewUsuarios.Columns["DescripcionRol"].HeaderText = "Role Description";
 
@@ -62,21 +61,29 @@ namespace CapaPresentacion
                 dataGridViewUsuarios.Columns["IdUsuario"].Width = 100;
                 dataGridViewUsuarios.Columns["DOCUMENTO"].Width = 100;
                 dataGridViewUsuarios.Columns["NOMBRECOMPLETO"].Width = 150;
-                dataGridViewUsuarios.Columns["CORREO"].Width = 250;
                 dataGridViewUsuarios.Columns["CLAVE"].Width = 150;
+                dataGridViewUsuarios.Columns["CORREO"].Width = 250;
                 dataGridViewUsuarios.Columns["DescripcionRol"].Width = 200;
 
                 // Alinear a la izquierda las columnas deseadas
                 dataGridViewUsuarios.Columns["IdUsuario"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
                 dataGridViewUsuarios.Columns["DOCUMENTO"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
                 dataGridViewUsuarios.Columns["NOMBRECOMPLETO"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
-                dataGridViewUsuarios.Columns["CORREO"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
                 dataGridViewUsuarios.Columns["CLAVE"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+                dataGridViewUsuarios.Columns["CORREO"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
                 dataGridViewUsuarios.Columns["DescripcionRol"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
 
                 // Evitar que las columnas se redimensionen con el mouse
                 dataGridViewUsuarios.AllowUserToResizeColumns = false;
                 dataGridViewUsuarios.AllowUserToResizeRows = false;
+
+                //Reordenar Las Columas En El DataGridView
+                dataGridViewUsuarios.Columns["IdUsuario"].DisplayIndex = 0; // Columna 1
+                dataGridViewUsuarios.Columns["DOCUMENTO"].DisplayIndex = 1; // Columna 2
+                dataGridViewUsuarios.Columns["NOMBRECOMPLETO"].DisplayIndex = 2; // Columna 3
+                dataGridViewUsuarios.Columns["CLAVE"].DisplayIndex = 3; // Columna 4
+                dataGridViewUsuarios.Columns["DescripcionRol"].DisplayIndex = 4; // Columna 5
+                dataGridViewUsuarios.Columns["CORREO"].DisplayIndex = 5; // Columna 6
 
             }
             catch (Exception ex)
